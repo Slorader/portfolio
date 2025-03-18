d19ff1cd6a630388170606caeaaa874fe58ac0ac
Léo TRUX <truxleo@gmail.com>
2025-03-18 17:14:00 +0100

    up

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,32 +1,29 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.scss";
 import Scroll from "@/components/ui/scroll";
 import React from "react";
 import {Providers} from "@/app/[locale]/providers";
 
 export const metadata: Metadata = {
-  title: "Léo TRUX - Portfolio",
-  description: "Leo's Portfolio ",
+    title: "Léo TRUX - Portfolio",
+    description: "Leo's Portfolio ",
 };
 
 
-export default function RootLayout({
-  children,
-  params,
-}: Readonly<{
-  children: React.ReactNode;
-  params: {
-    locale: string;
-  }
-}>) {
-  return (
-    <html lang="en">
-    <body>
-    <div className="mx-auto max-w-150 z-2 flex flex-col justify-center">
-      <Providers local={params.locale}>{children}</Providers>
-      <Scroll/>
-    </div>
-    </body>
-    </html>
-);
+export default async function RootLayout({children, params}: {
+    children: React.ReactNode;
+    params: Promise<{ locale: string }>
+}) {
+    const {locale} = await params;
+    return (
+
+        <html lang="en">
+        <body>
+        <div className="mx-auto max-w-150 z-2 flex flex-col justify-center">
+            <Providers local={locale}>{children}</Providers>
+            <Scroll/>
+        </div>
+        </body>
+        </html>
+    );
 }
