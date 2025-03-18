ad50aa72da85c98841b506695f638c42fdce34ee
Léo TRUX <truxleo@gmail.com>
2025-03-18 19:17:55 +0100

    up

---
+++ app/[locale]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+    return (
+        <div className="fixed z-100 flex justify-center items-center top-0 left-0 w-full h-full bg-white">
+            <div className="loader"></div>
+        </div>
+    );
+}
+
