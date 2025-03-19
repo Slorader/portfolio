6c9908c82e7e7779a9fe4297169880cbf1d7e257
Léo TRUX <truxleo@gmail.com>
2025-03-19 11:48:46 +0100

    up

--- components/layout/contact.tsx
+++ components/layout/contact.tsx
@@ -2,23 +2,56 @@
 import Input from "@/components/ui/form/input";
 import Textarea from "@/components/ui/form/textarea";
 import Button from "@/components/ui/form/button";
-import Form from "next/form";
 import { useI18n } from "@/locales/client";
+import React, {FormEvent} from "react";
 
-async function sendEmail() {
-    console.log('email send');
-}
-
-export default  function Contact() {
+export default function Contact() {
     const t =  useI18n();
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [error, setError] = React.useState<string | null>(null);
+
+    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        setIsLoading(true);
+        event.preventDefault();
+
+        try {
+            const formData: FormData = new FormData(event.currentTarget);
+            const data = {
+                'message': formData.get('message'),
+                'name': formData.get('name'),
+                'email': formData.get('email')
+            };
+
+            const response = await fetch("/api/resend", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (response.ok) {
+                console.log(error)
+            }
+
+        } catch (error) {
+            if (error instanceof Error) {
+                setError(error.message);
+                console.error(error);
+            }
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
-        <Form onSubmit={sendEmail} action={""}>
+        <form onSubmit={onSubmit}>
             <div className="flex w-full gap-2 mb-2">
                 <Input id="name" type="text" name="name" label={t("contact.name")} />
                 <Input id="email" type="email" name="email" label={t("contact.email")} />
             </div>
             <Textarea id="message" name="message" label={t("contact.message")} />
-            <Button  id="button" name="button" label={t("contact.submit")} />
-        </Form>
+            <Button  id="button" name="button" label={t("contact.submit")} isLoading={isLoading} />
+        </form>
     );
 }
--- components/ui/form/button.tsx
+++ components/ui/form/button.tsx
@@ -1,10 +1,13 @@
+import React from "react";
+
 interface ButtonProps {
     id: string;
     name: string;
     label: string;
+    isLoading: boolean;
 }
 
-export default function Button({ id, name, label }: ButtonProps) {
+export default function Button({id, name, label, isLoading}: ButtonProps) {
     return (
         <button
             type="submit"
@@ -12,7 +15,8 @@ export default function Button({ id, name, label }: ButtonProps) {
             name={name}
             className="active:scale-95 w-full transition duration-300 ease-in-out flex items-center cursor-pointer bg-gradient-to-br text-white font-bold from-[#3e31fa] to-[#825df6] rounded-[10px] w-50 justify-center px-5 h-15"
         >
-            {label}
+            {isLoading && (<div className="loader"></div>)}
+            {!isLoading && label}
         </button>
     );
 }
