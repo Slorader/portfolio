22107da4ec7d70a8ef327313ce77cab7b3e53994
Léo TRUX <truxleo@gmail.com>
2025-03-19 11:54:09 +0100

    up

--- app/api/resend/route.ts
+++ app/api/resend/route.ts
@@ -1,9 +1,9 @@
-import {NextResponse} from "next/server";
+import {NextRequest} from "next/server";
 import {Resend} from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request: NextResponse) {
+export async function POST(request: NextRequest) {
     const email = process.env.MY_EMAIL_ADDRESS;
     const dataForm = await request.json();
 
