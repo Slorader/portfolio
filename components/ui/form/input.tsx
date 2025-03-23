ab4fd91f8845f9987adc3aaca22627e9bbef0f0f
Léo TRUX <truxleo@gmail.com>
2025-03-23 14:40:55 +0100

    up

--- components/ui/form/input.tsx
+++ components/ui/form/input.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
+import {IoMdClose} from "react-icons/io";
 
 interface InputProps {
     id: string;
@@ -7,12 +8,14 @@ interface InputProps {
     label: string;
     isError: boolean;
     value?: string;
+    resetSpecificField: (field: "name" | "message" | "email" ) => void;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({ id, name, type, label, isError, value = "", onChange, ...register }: InputProps) {
+export default function Input({id, name, type, label, isError, value = "", resetSpecificField, onChange, ...register}: InputProps) {
     const [hasValue, setHasValue] = useState<boolean>(value !== "");
     const [inputValue, setInputValue] = useState<string>(value);
+    const [displayIcon, setDisplayIcon] = useState<boolean>(false);
 
     useEffect(() => {
         setHasValue(inputValue !== "");
@@ -25,6 +28,16 @@ export default function Input({ id, name, type, label, isError, value = "", onCh
         }
     };
 
+    const handleBlur = () => {
+        setTimeout(() => setDisplayIcon(false), 100);
+    }
+
+    function removeValue(): void {
+        resetSpecificField(id === "name" ? "name" : "message");
+        setHasValue(false);
+        setInputValue("");
+    }
+
     return (
         <div className="relative w-full h-15 group">
             <input
@@ -34,6 +47,8 @@ export default function Input({ id, name, type, label, isError, value = "", onCh
                 className={` ${isError ? "border-[var(--form-error)]" : "border-transparent border-[var(--main-color)] "} z-20 relative w-full h-15 transition duration-300 ease-in-out border-2 border-solid focus:border-2 focus:bg-white focus:border-[var(--main-color)] pt-4 pl-4 pr-10 rounded-[10px] outline-none bg-[var(--form-bg)]`}
                 name={name}
                 type={type}
+                onFocus={() => setDisplayIcon(true)}
+                onBlurCapture={handleBlur}
                 {...register}
             />
             <label
@@ -41,6 +56,10 @@ export default function Input({ id, name, type, label, isError, value = "", onCh
                 className={`${hasValue ? "top-4 text-xs" : ""} z-20 group-focus-within:text-xs group-focus-within:top-4 text-[#5e6a83] absolute left-0 top-1/2 transform -translate-y-1/2 pl-4 transition-all duration-300 ease-in-out`}>
                 {label}
             </label>
+            <IoMdClose fontSize={25}
+                       onClick={removeValue}
+                       className={` ${displayIcon ? "z-50" : "z-10"} absolute right-3 top-1/2 transform -translate-y-1/2 text-[#5e6a83] cursor-pointer transition duration-300 ease-in-out opacity-0 group-focus-within:opacity-100`}
+            />
         </div>
     );
 }
--- components/ui/form/textarea.tsx
+++ components/ui/form/textarea.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import {IoMdClose} from "react-icons/io";
 
 interface TextareaProps {
     id: string;
@@ -6,13 +7,15 @@ interface TextareaProps {
     label: string;
     isError: boolean;
     value?: string;
+    resetSpecificField: (field: "name" | "message" | "email" ) => void;
     onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-export default function Textarea({id, name, label, isError, value = "", onChange, ...register}: TextareaProps) {
+export default function Textarea({id, name, label, isError, value = "", resetSpecificField, onChange, ...register}: TextareaProps) {
     const [hasValue, setHasValue] = useState<boolean>(false);
     const [changeBg, setChangeBg] = useState<boolean>(false);
     const [inputValue, setInputValue] = useState<string>(value);
+    const [displayIcon, setDisplayIcon] = useState<boolean>(false);
 
     useEffect(() => {
         setHasValue(inputValue !== "");
@@ -26,6 +29,22 @@ export default function Textarea({id, name, label, isError, value = "", onChange
         }
     }
 
+    const handleFocus = () => {
+        setChangeBg(true);
+        setDisplayIcon(true);
+    }
+
+    const handleBlur = () => {
+        setChangeBg(false);
+        setTimeout(() => setDisplayIcon(false), 100);
+    }
+
+    const removeValue= () => {
+        resetSpecificField("message");
+        setHasValue(false);
+        setInputValue("");
+    }
+
     return (
         <div className="relative w-full group">
             <textarea
@@ -34,12 +53,16 @@ export default function Textarea({id, name, label, isError, value = "", onChange
                 onChange={handleChange}
                 className={` ${isError ? "border-[var(--form-error)]" : "border-[var(--main-color)] border-transparent "} resize-none z-20 relative w-full h-[200px] transition duration-300 ease-in-out border-2 border-solid  focus:border-2 focus:bg-white focus:border-[var(--main-color)] pt-7 pl-4 pr-8 rounded-[10px] outline-none bg-[var(--form-bg)]`}
                 name={name}
-                onFocus={() => setChangeBg(true)}
-                onBlurCapture={() => setChangeBg(false)}
+                onFocus={handleFocus}
+                onBlurCapture={handleBlur}
                 {...register}
             />
             <label htmlFor={id}
                    className={`${hasValue ? "top-4 text-xs" : "top-7"} ${changeBg ? "bg-white" : "bg-[var(--form-bg)]"} z-20 w-138 h-7 flex items-center group-focus-within:text-xs group-focus-within:top-4 text-[#5e6a83] absolute left-0 transform -translate-y-1/2 ml-4 transition-all duration-300 ease-in-out`}>{label}</label>
+            <IoMdClose fontSize={25}
+                       onClick={removeValue}
+                       className={` ${displayIcon ? "z-50" : "z-10"} cursor-pointer transition duration-300 ease-in-out group-focus-within:opacity-100 opacity-0 absolute right-3 top-3 font-size text-[#5e6a83]`}
+            />
         </div>
     );
 }
