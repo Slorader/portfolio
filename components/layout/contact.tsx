8b02ea013611a5320f9d095ec43ac9ed8c966368
Léo TRUX <truxleo@gmail.com>
2025-03-23 11:44:27 +0100

    up

--- components/layout/contact.tsx
+++ components/layout/contact.tsx
@@ -1,39 +1,66 @@
 "use client";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {z} from "zod";
 import Input from "@/components/ui/form/input";
 import Textarea from "@/components/ui/form/textarea";
 import Button from "@/components/ui/form/button";
-import { useI18n } from "@/locales/client";
-import React, {FormEvent} from "react";
+import {useScopedI18n} from "@/locales/client";
+import {RiErrorWarningLine} from "react-icons/ri";
+import React, {useEffect, useState} from "react";
+
+
 
 export default function Contact() {
-    const t =  useI18n();
-    const [isLoading, setIsLoading] = React.useState(false);
-    const [error, setError] = React.useState<string | null>(null);
+    const t = useScopedI18n('form');
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
 
-    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    const formSchema = z.object({
+        name: z.string().min(3, {
+            message: t("message.name"),
+        }),
+        email: z.string().email({
+            message: t("message.email"),
+        }),
+        message: z.string().min(1, {
+            message: t("message.message"),
+        }),
+    });
+
+    type FormSchema = z.infer<typeof formSchema>;
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+        watch
+    } = useForm<FormSchema>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            message: ""
+        }
+    });
+
+    const onSubmit = async (data: FormSchema) => {
         setIsLoading(true);
-        event.preventDefault();
+        setError(null);
 
         try {
-            const formData: FormData = new FormData(event.currentTarget);
-            const data = {
-                'message': formData.get('message'),
-                'name': formData.get('name'),
-                'email': formData.get('email')
-            };
-
             const response = await fetch("/api/resend", {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
+                headers: {"Content-Type": "application/json"},
                 body: JSON.stringify(data),
             });
 
             if (response.ok) {
-                console.log(error)
+                reset();
+            } else {
+                setError("erreur lors de l'envoi");
             }
-
         } catch (error) {
             if (error instanceof Error) {
                 setError(error.message);
@@ -42,16 +69,37 @@ export default function Contact() {
         } finally {
             setIsLoading(false);
         }
-    }
+    };
+
+    useEffect(() => {
+        const subscription = watch((value) => console.log("Valeurs du formulaire (contact) :", value));
+        return () => subscription.unsubscribe();
+    }, [watch]);
+
 
     return (
-        <form onSubmit={onSubmit}>
-            <div className="flex w-full gap-2 mb-2">
-                <Input id="name" type="text" name="name" label={t("contact.name")} />
-                <Input id="email" type="email" name="email" label={t("contact.email")} />
-            </div>
-            <Textarea id="message" name="message" label={t("contact.message")} />
-            <Button  id="button" name="button" label={t("contact.submit")} isLoading={isLoading} />
-        </form>
+        <>
+            <form className="relative" onSubmit={handleSubmit(onSubmit)}>
+                {error && (
+                    <div className="absolute z-2 -top-8 flex w-full items-center">
+                        <RiErrorWarningLine className="mr-2 text-red-500" size={25}/>
+                        <span className="text-red-500 mt-1">{error}</span>
+                    </div>
+                )}
+                <div className="flex w-full gap-2">
+                    <div className="flex flex-col w-1/2 mb-2">
+                        <Input id="name" type="text" {...register("name")} label={t("fields.name")} isError={!!errors.name}/>
+                        <span className={` ${errors.name ? "opacity-100" : "opacity-0" } text-[var(--form-error)] text-xs mb-1 mt-1 h-2`}>{errors.name?.message}</span>
+                    </div>
+                    <div className="flex flex-col w-1/2">
+                        <Input id="email" type="text" {...register("email")} label={t("fields.email")} isError={!!errors.email}/>
+                        <span className={` ${errors.email ? "opacity-100" : "opacity-0" } text-[var(--form-error)] text-xs mb-1 mt-1 h-2`}>{errors.email?.message}</span>
+                    </div>
+                </div>
+                <Textarea id="message" {...register("message")} label={t("fields.message")} isError={!!errors.message}/>
+                <span className={` ${errors.message ? "opacity-100" : "opacity-0" } text-[var(--form-error)] text-xs mb-1  absolute mt-1 h-2`}>{errors.message?.message}</span>
+                <Button id="button" name="button" label={t("fields.submit")} isLoading={isLoading}/>
+            </form>
+        </>
     );
 }
--- components/ui/form/input.tsx
+++ components/ui/form/input.tsx
@@ -1,57 +1,48 @@
-"use client";
-import {IoMdClose} from "react-icons/io";
-import {RefObject, useEffect, useRef, useState} from "react";
+import React, { useState, useEffect } from "react";
 
 interface InputProps {
     id: string;
     name: string;
     type: string;
     label: string;
+    isError: boolean;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({id, name, type, label}: InputProps) {
-    const [hasValue, setHasValue] = useState<boolean>(false);
-    const inputRef: RefObject<HTMLInputElement | null> = useRef<HTMLInputElement>(null);
-    const [displayIcon, setDisplayIcon] = useState<boolean>(false);
+export default function Input({ id, name, type, label, isError, value = "", onChange, ...register }: InputProps) {
+    const [hasValue, setHasValue] = useState<boolean>(value !== "");
+    const [inputValue, setInputValue] = useState<string>(value);
 
-    useEffect((): void => {
-        if (inputRef.current && inputRef.current.value !== "") {
-            setHasValue(true);
+    useEffect(() => {
+        setHasValue(inputValue !== "");
+    }, [inputValue]);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+        if (onChange) {
+            onChange(e);
         }
-        const handleFocus = () => setDisplayIcon(true);
-        const handleBlur = () => {
-            setTimeout(() => setDisplayIcon(false), 100);
-        };
-        inputRef.current?.addEventListener("focus", handleFocus);
-        inputRef.current?.addEventListener("blur", handleBlur);
-
-    }, []);
-
-    function removeValue(): void {
-        setHasValue(false);
-        if (inputRef.current) {
-            inputRef.current.value = "";
-        }
-    }
+    };
 
     return (
-        <div className="relative w-1/2 h-15 group">
-            <input ref={inputRef}
-                   id={id}
-                   onChange={(e) => setHasValue(e.target.value !== "")}
-                   className="z-20 relative w-full h-15 transition duration-300 ease-in-out border-transparent border-2 border-solid border-[var(--main-color)] focus:border-2 focus:bg-white focus:border-[var(--main-color)] pt-4 pl-4 pr-10 rounded-[10px] outline-none bg-[var(--form-bg)]"
-                   name={name}
-                   type={type}/>
-
-            <label htmlFor={id}
-                   className={`${hasValue ? "top-4 text-xs" : ""} z-20 group-focus-within:text-xs group-focus-within:top-4 text-[#5e6a83] absolute left-0 top-1/2 transform -translate-y-1/2 pl-4 transition-all duration-300 ease-in-out`}>
+        <div className="relative w-full h-15 group">
+            <input
+                id={id}
+                value={inputValue}
+                onChange={handleChange}
+                className={` ${isError ? "border-[var(--form-error)]" : "border-transparent border-[var(--main-color)] "} z-20 relative w-full h-15 transition duration-300 ease-in-out border-2 border-solid focus:border-2 focus:bg-white focus:border-[var(--main-color)] pt-4 pl-4 pr-10 rounded-[10px] outline-none bg-[var(--form-bg)]`}
+                name={name}
+                type={type}
+                {...register}
+            />
+            <label
+                htmlFor={id}
+                className={`${hasValue ? "top-4 text-xs" : ""} z-20 group-focus-within:text-xs group-focus-within:top-4 text-[#5e6a83] absolute left-0 top-1/2 transform -translate-y-1/2 pl-4 transition-all duration-300 ease-in-out`}>
                 {label}
             </label>
-
-            <IoMdClose fontSize={25}
-                       onClick={removeValue}
-                       className={` ${displayIcon ? "z-50" : "z-10"} absolute right-3 top-1/2 transform -translate-y-1/2 text-[#5e6a83] cursor-pointer transition duration-300 ease-in-out opacity-0 group-focus-within:opacity-100`}
-            />
         </div>
     );
 }
+
+
